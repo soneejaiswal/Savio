44906d6aaf9630752b1e9b1222b1d24fba964d2e
sonee jaiswal <soneejaiswal@gmail.com>
2016-05-18 10:08:48 +0530

    working project module is created

--- savio-controller/src/main/java/com/vatsa/savio/controller/PartyController.java
+++ savio-controller/src/main/java/com/vatsa/savio/controller/PartyController.java
@@ -1,5 +1,7 @@
 package com.vatsa.savio.controller;
 
+import java.io.IOException;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -7,6 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vatsa.savio.core.constants.PartyUriConstants;
@@ -19,17 +22,13 @@ public class PartyController {
 	/*@Autowired
 	private CustomerService customerService;*/
 
-	/*@RequestMapping(value = CustomerUriConstants.REGISTER_CUSTOMER, method = RequestMethod.POST, headers = "content-type=application/json")*/
 
 	@RequestMapping(value = PartyUriConstants.REGISTER_PARTY, method = RequestMethod.GET)
-	public String saveParty(HttpServletRequest req, HttpServletResponse resp) {
-		
+	public @ResponseBody String saveParty(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
 		log.trace("test log");
 		System.out.println("sysout test log");
 		
-		req.setAttribute("responseMsg", "Query string parameter 'app-id' not specified");
-		
-		
-		return "response" ;
+		return "response body test string" ;
 	}
 }
